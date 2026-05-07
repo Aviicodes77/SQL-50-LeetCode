bf78df654a47c230fbfa92c12c4800f5ae267bc1
Avnish Choudhary <aviicodes00@gmail.com>
2026-05-07 13:21:53 +0530

    Time: 351 ms (59.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,39 @@
+# Write your MySQL query statement below
+WITH daily_sales AS (
+    SELECT 
+        visited_on,
+        SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY visited_on
+),
+
+moving_avg AS (
+    SELECT
+        visited_on,
+
+        SUM(daily_amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) AS amount,
+
+        ROUND(
+            AVG(daily_amount) OVER (
+                ORDER BY visited_on
+                ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+            ),
+            2
+        ) AS average_amount,
+
+        ROW_NUMBER() OVER (ORDER BY visited_on) AS rn
+
+    FROM daily_sales
+)
+
+SELECT 
+    visited_on,
+    amount,
+    average_amount
+FROM moving_avg
+WHERE rn >= 7
+ORDER BY visited_on;
+
